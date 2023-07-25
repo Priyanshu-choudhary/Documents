405e03948cebc0ef2ca640c31422d97ce5914950
Priyanshu-choudhary <broyadi23@gmail.com>
2023-07-25 14:46:47 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -1,4 +1,43 @@
 #!/bin/sh
+
+#ARDUINO FOLDER
+cd /home/priyanshu/arduino
+echo "backup Start...."
+git init
+git add -A
+git commit -m "backup"
+git remote add 
+git push -u origin master
+echo "backup complete......"
+
+#JAVA FOLDER
+cd /home/priyanshu/java
+echo "backup Start...."
+git init
+git add -A
+git commit -m "backup"
+git push -u origin master
+echo "backup complete......"
+
+#C-LANGUAGE FOLDER
+cd /home/priyanshu/c-language
+echo "backup Start...."
+git init
+git add -A
+git commit -m "backup"
+git push -u origin master
+echo "backup complete......"
+
+#SHELL FOLDER
+cd /home/priyanshu/shell
+echo "backup Start...."
+git init
+git add -A
+git commit -m "backup"
+git push -u origin master
+echo "backup complete......"
+
+#DOCUMENTS FOLDER
 cd /home/priyanshu/Documents
 echo "backup Start...."
 git init
@@ -6,3 +45,4 @@ git add -A
 git commit -m "backup"
 git push -u origin master
 echo "backup complete......"
+
