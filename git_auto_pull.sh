a0ae225dd7b51b3d48cb56fd688f4af35370fa22
yadi choudhary <84007868+Priyanshu-choudhary@users.noreply.github.com>
2023-08-29 01:33:00 +0530

    Update git_auto_pull.sh

--- git_auto_pull.sh
+++ git_auto_pull.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 cd /home/priyanshu/
+git init
+git config -- global user.email”broyadi23@gmail.ocm”
+git config -- global user.name”Priyanshu-choudhary”
+
 if [ `ping -c 1 -q google.com >&/dev/null; echo $?` == 0 ]
 then
    rm -f -r java
