5cd341a0fbf2c9d9eee323d07938f9ba23488287
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-29 15:51:11 +0530

    Tue 29 Aug 15:51:01 IST 2023

--- yes-command.sh
+++ yes-command.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 echo "do you want to continue?"
 read answer
-if [ $answer == y  ]
+if [ $answer == y ]
 then
    echo "hello"
+elif [ $answer == /n ]
+then
+  echo "enter"
+
 else
    echo "Not working"
 
