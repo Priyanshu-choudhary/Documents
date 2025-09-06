9bf30d4031b45605fa6cd6ca0c0a65aa38bc0a60
yadi choudhary <84007868+Priyanshu-choudhary@users.noreply.github.com>
2025-09-06 16:43:15 +0530

    Update git_config.sh

--- git_config.sh
+++ git_config.sh
@@ -1,10 +1,9 @@
 #!bin/sh
 git config --global user.name ”Priyanshu-choudhary”
 git config --global user.email ”broyadi23@gmail.com”
-git config --global color.ui true (enables code highlights)
-git config --list     //give information about username
+git config --list   
 
 ssh-keygen -t ed25519 -C "broyadi23@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-clip < ~/.ssh/id_ed25519.pub
+
